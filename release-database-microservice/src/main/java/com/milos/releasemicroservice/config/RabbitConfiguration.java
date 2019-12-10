333d4be45dc5512cef7a2f7120a2ba84d147b0b8
Milos <ger.milos.bt@gmail.com>
2019-12-10 16:55:18 +0100

    Working on rabbitmq dto transfer

--- release-database-microservice/src/main/java/com/milos/releasemicroservice/config/RabbitConfiguration.java
+++ release-database-microservice/src/main/java/com/milos/releasemicroservice/config/RabbitConfiguration.java
@@ -1,5 +1,6 @@
 package com.milos.releasemicroservice.config;
 
+import com.milos.releasemicroservice.util.QueueConsumer;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
@@ -7,6 +8,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.BlockingQueueConsumer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,43 +18,49 @@ import org.springframework.amqp.core.Binding;
 public class RabbitConfiguration
 {
 	private static final String LISTENER_METHOD = "receiveMessage";
-	@Value("${queue.name}")
-	private String queueName;
-	@Value("${fanout.exchange}")
-	private String fanoutExchange;
 
-	@Bean
-	Queue queue()
-	{
-		return new Queue(queueName, true);
-	}
-
-	@Bean
-	FanoutExchange exchange()
-	{
-		return new FanoutExchange(fanoutExchange);
-	}
-
-	@Bean
-	Binding binding(Queue queue, FanoutExchange exchange)
-	{
-		return BindingBuilder.bind(queue).to(exchange);
-	}
-
-	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-			MessageListenerAdapter listenerAdapter)
-	{
-		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames(queueName);
-		container.setMessageListener(listenerAdapter);
-		return container;
-	}
-
-	@Bean
-	MessageListenerAdapter listenerAdapter(BlockingQueueConsumer consumer)
-	{
-		return new MessageListenerAdapter(consumer, LISTENER_METHOD);
-	}
+//	@Value("${queue.name}")
+//	private String queueName;
+//	@Value("${fanout.exchange}")
+//	private String fanoutExchange;
+//
+//	@Bean
+//	Queue queue()
+//	{
+//		return new Queue(queueName, true);
+//	}
+//
+//	@Bean
+//	FanoutExchange exchange()
+//	{
+//		return new FanoutExchange(fanoutExchange);
+//	}
+//
+//	@Bean
+//	Binding binding(Queue queue, FanoutExchange exchange)
+//	{
+//		return BindingBuilder.bind(queue).to(exchange);
+//	}
+//
+//	@Bean
+//	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+//			MessageListenerAdapter listenerAdapter)
+//	{
+//		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+//		container.setConnectionFactory(connectionFactory);
+//		container.setQueueNames(queueName);
+//		container.setMessageListener(listenerAdapter);
+//		return container;
+//	}
+//
+//	@Bean
+//	public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+//		return new Jackson2JsonMessageConverter();
+//	}
+//
+//	@Bean
+//	MessageListenerAdapter listenerAdapter(QueueConsumer consumer)
+//	{
+//		return new MessageListenerAdapter(consumer, LISTENER_METHOD);
+//	}
 }
--- release-database-microservice/src/main/java/com/milos/releasemicroservice/util/QueueConsumer.java
+++ release-database-microservice/src/main/java/com/milos/releasemicroservice/util/QueueConsumer.java
@@ -1,28 +1,28 @@
 package com.milos.releasemicroservice.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Slf4j
 public class QueueConsumer
 {
-	@Autowired
-	public void receiveMessage(String message)
+	public void receiveMessage(List<ImageDTO> message)
 	{
 		log.info("Received (String) " + message);
 		processMessage(message);
 	}
 
-	public void receiveMessage(byte[] message)
-	{
-		String strMessage = new String(message);
-		log.info("Received (No String) " + strMessage);
-		processMessage(strMessage);
-	}
+//	public void receiveMessage(byte[] message)
+//	{
+//		String strMessage = new String(message);
+//		log.info("Received (No String) " + strMessage);
+//		processMessage(strMessage);
+//	}
 
-	private void processMessage(String message)
+	private void processMessage(List<ImageDTO> message)
 	{
 		try
 		{
